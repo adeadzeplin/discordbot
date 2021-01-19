9c3660d85356c08402234e5321d32ee3516033e6
adeadzeplin <smithdepazd@gmail.com>
2021-01-19 16:26:48 -0500

    fixed admin commands

--- lib/cmds/misc.py
+++ lib/cmds/misc.py
@@ -19,7 +19,8 @@ def king(bot,user,*args):
         }
         bot.PIPES['t2s']['data'].put(data)
     elif args[0] == 'join' or (user['name'] == 'adeadzeplin'):
-        bot.send_message(f"{user['name']} Joined king of the hill")
+        if args[0] == 'join':
+            bot.send_message(f"{user['name']} Joined king of the hill")
         data = {
             'user': user,
             'args': args
