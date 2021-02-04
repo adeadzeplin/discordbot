e8e99a4a2a365927144b3cd564288eee166a1d0f
adeadzeplin <smithdepazd@gmail.com>
2021-02-04 18:43:48 -0500

    Merge remote-tracking branch 'origin/master'

--- cogs/benis.py
+++ cogs/benis.py
@@ -9,18 +9,16 @@ class Benis(commands.Cog):
         self.guild = self.client.get_guild(437778883744628736)
         self.role = self.guild.get_role(757721456057516073)  # benis role
 
-    @commands.command(aliases = ['b'])
+    @commands.command()
     async def benis(self,ctx,target=None):
         if target != None:
             temp = int(target.strip('<!@>'))
             user = self.client.get_user(temp)
         else:
             user = ctx.message.author
-
         if ctx.guild == self.guild:
             await self.purgerole(self.role)
             await self.giveuserole(user,self.role)
-
 
     async def purgerole(self,role): # Removes every member from a given role
         for rolemember in role.members:
